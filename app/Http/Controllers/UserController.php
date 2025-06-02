c7dc3d42967f0f7b10d31b3ed4fc0343663eeb92
Rafi Anshory <mrafianshory@gmail.com>
2025-06-02 12:43:46 +0000

    API for users data

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::get();
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $users = new User();
+        $users->name = $request->name;
+        $users->email = $request->email;
+        $users->password = $request->password;
+        $users->save();
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $users = User::find($id);
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $users = User::find($id);
+        $users->name = $request->name;
+        $users->email = $request->email;
+        $users->password = $request->password;
+        $users->save();
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        User::destroy($id);
+        return response()->json(['message'=>'Deleted'], 200);
+    }
+}
